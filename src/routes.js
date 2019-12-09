71b6ab1cde75177c04e06ff45f54ab2b2c28b924
Bruno S Bertolani <brunosbertolani@gmail.com>
2019-12-09 20:42:24 -0300

    add model and controller for student

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import Student from './app/models/Student';
+
+import StudentController from './app/controllers/StudentController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+    return res.json({ message: 'Hello World' });
 });
+
+routes.post('/students', StudentController.store);
 
 export default routes;
