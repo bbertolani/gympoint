47452f3712cbd75fe128496a603441b97bd6487d
Bruno S Bertolani <brunosbertolani@gmail.com>
2019-12-28 14:29:41 -0300

    check if a user already exist

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -2,6 +2,14 @@ import Student from '../models/Student';
 
 class StudentController {
     async store(req, res) {
+        const userExist = await Student.findOne({
+            where: { email: req.body.email },
+        });
+
+        if (userExist) {
+            return res.status(401).json({ error: 'User already exist' });
+        }
+
         const student = await Student.create(req.body);
         return res.json(student);
     }
