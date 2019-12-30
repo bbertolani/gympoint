364003e3d9deb3bceca7055d00c0c403589e1777
Bruno S Bertolani <brunosbertolani@gmail.com>
2019-12-30 20:58:46 -0300

    Fix review

--- src/database/migrations/20191207165236-students.js
+++ src/database/migrations/20191207165236-students.js
@@ -44,6 +44,6 @@ module.exports = {
     },
 
     down: (queryInterface, Sequelize) => {
-        return queryInterface.dropTable('users');
+        return queryInterface.dropTable('students');
     },
 };
